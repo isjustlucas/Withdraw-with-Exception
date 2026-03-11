79b9bbcf121afe30d8df3743a0c0baa240a22574
isjustlucas <vastokinha100@gmail.com>
2026-03-11 17:23:56 -0300

    Live

---
+++ src/app/App.java
@@ -0,0 +1,39 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import models.entities.Account;
+import models.exceptions.AccountException;
+
+public class App {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        try {
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Initial balance: ");
+        double initialBalance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+        Account account = new Account(number, holder, initialBalance, withdrawLimit);
+        
+        System.out.println();
+        System.out.print("Enter amount for withdraw: ");
+        double withdrawAmount = sc.nextDouble();
+        account.withdraw(withdrawAmount);
+        System.out.println("New balance: " + String.format("%.2f", account.getBalance()));
+        } catch (AccountException e) {
+            System.out.println(e.getMessage());
+        } catch (RuntimeException e){
+            System.out.println("Unexpected error");
+        }
+        sc.close();
+    }
+}
---
+++ src/models/exceptions/AccountException.java
@@ -0,0 +1,9 @@
+package models.exceptions;
+
+public class AccountException extends RuntimeException {
+
+    public AccountException(String message) {
+        super(message);
+    }
+    
+}
